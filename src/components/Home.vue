65556265f435bfd1c274d201004bdc268c7e55f1
yanxitao <136620970@qq.com>
2019-02-19 19:04:31 +0800

    项目提交

---
+++ src/components/Home.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="bgView">
+        
+    </div>
+</template>
+
+<style>
+    .bgView {
+        height: 100vh;
+        width: 100vw;
+        background-color: aquamarine;
+    }
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import AntiAudit from '@/components/AntiAudit'
+import Home from '@/components/Home'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    // {
+    //   path: '/',
+    //   name: 'HelloWorld',
+    //   component: HelloWorld,
+    // },
+    {
+      path: '/',
+      name: 'Home',
+      component: Home
+    },
+
+  ]
+})
